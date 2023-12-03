67875b840679bb84626cc591884a2f60e63b3aa8
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-12-03 18:03:47 +0530

    added images via admin upload

--- project/blog/models.py
+++ project/blog/models.py
@@ -24,7 +24,7 @@ class AuthorModel(models.Model):
 class PostModel(models.Model):
     title = models.CharField(max_length=50)
     excerpt = models.CharField(max_length=150)
-    img_name = models.CharField(max_length=20)
+    img_name = models.ImageField(upload_to='posts',null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True,db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])

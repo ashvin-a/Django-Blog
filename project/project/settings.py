aa5bbb8d67bfd9d3ba91a521876c0899a9fb3b23
Ashvin <ashvin.anilkumar@qburst.com>
2023-12-04 11:27:10 +0530

    all set for deployement

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
